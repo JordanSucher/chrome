8331b525d816a6742ffd0e6d3da144066eafffdb
JordanSucher <jsucher@gmail.com>
2023-08-21 16:21:39 -0600

    modified start script

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ set -e
 _kill_procs() {
   kill -TERM $node
   kill -TERM $xvfb
+  kill -TERM $chrome  # Add this line to ensure Chrome also gets killed on termination signals
 }
 
 # Relay quit commands to processes
@@ -18,8 +19,13 @@ xvfb=$!
 
 export DISPLAY=:99
 
+# Start Chrome with remote debugging enabled and other necessary flags
+google-chrome-stable --headless --remote-debugging-port=9222 --no-sandbox --disable-dev-shm-usage &
+chrome=$!  # Store the PID of the Chrome process for later
+
 dumb-init -- node ./build/index.js $@ &
 node=$!
 
 wait $node
 wait $xvfb
+wait $chrome  # Wait for Chrome to exit before exiting the script
